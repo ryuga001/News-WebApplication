05519eb42e28776396deb5ce641d0bbf9559b6ca
RAHUL SINGH <rahul0singh001@gmail.com>
2023-04-29 01:49:23 +0530

    navigation through router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,47 @@
 import './App.css';
 // class based components-->
 
-import React , {Component} from 'react'
+import React, { Component } from 'react'
 import { NavBar } from './components/NavBar';
 import { News } from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 export default class App extends Component {
-    render() {
-      return (
-        <div>
-          <NavBar/>
-          <News pageSize={8} country={"in"} category={"technology"} />
-          <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+  render() {
+    return (
+      <div>
+          <Router>
+          <NavBar />
+          <Routes>
+          <Route exact path='/'
+            element={<News key="general" pageSize={8} country={"in"} category={"general"} />} >
+          </Route>
+          <Route exact path='/sport'
+            element={<News key="sport" pageSize={8} country={"in"} category={"sport"} />} >
+          </Route>
+          <Route exact path='/technology'
+            element={<News key="technology" pageSize={8} country={"in"} category={"technology"} />} >
+          </Route>
+          <Route exact path='/business'
+            element={<News key="business" pageSize={8} country={"in"} category={"business"} />} >
+          </Route>
+
+          <Route exact path='/entertainment'
+            element={<News key="entertainment" pageSize={8} country={"in"} category={"entertainment"} />} >
+          </Route>
+          <Route exact path='/health'
+            element={<News key="health" pageSize={8} country={"in"} category={"health"} />} >
+          </Route>
+          <Route exact path='/science'
+            element={<News key="science" pageSize={8} country={"in"} category={"science"} />} >
+          </Route>
+          </Routes>         
+      </Router>
         </div>
-      )
-    }
+    )
+  }
 }
